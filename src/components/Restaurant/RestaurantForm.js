4f300f4baabd480730a3b71a35ab064f8f185ee0
Scott Solmonson <ssolmonson63@gmail.com>
2020-09-03 06:57:15 -0400

    Add form to POST restaurants
    Created a form in order to properly create a restaurant. This form also
    includes a selection of categories for the 'foodtype' column. This will
    allow for a more streamlined way to define the food type and an easier
    recall of that information when that feature is to be added.
    
    Note: The categories defined for restaurants need to be organized and
    added/removed in a future version. This basic list was created in order
    to proceed with the rest of the application and push out a version one
    in a timely manner.

---
+++ src/components/Restaurant/RestaurantForm.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// Defined values for 'Food Type' to allow for consistent sorting
+const categoryData = [
+  { value: 'American', name: 'American' },
+  { value: 'Asian', name: 'Asian' },
+  { value: 'Cuban', name: 'Cuban' },
+  { value: 'Mexican', name: 'Mexican' },
+  { value: 'Bar', name: 'Bar' },
+  { value: 'Pub', name: 'Pub' },
+  { value: 'TextMex', name: 'TexMex' },
+  { value: 'Cafe', name: 'Cafe' },
+  { value: 'Fine Dining', name: 'Fine Dining' },
+  { value: 'Italian', name: 'Italian' },
+  { value: 'Greek', name: 'Greek' },
+  { value: 'Indian', name: 'Indian' },
+  { value: 'Middle Eastern', name: 'Middle Eastern' },
+  { value: 'Pizzeria', name: 'Pizzeria' },
+  { value: 'Bakery', name: 'Bakery' },
+  { value: 'Barbecue', name: 'Barbeque' },
+  { value: 'Cajun', name: 'Cajun' },
+  { value: 'Diner', name: 'Diner' },
+  { value: 'Gastropub', name: 'Gastropub' },
+  { value: 'Sandwich', name: 'Sandwich' },
+  { value: 'Fast-food', name: 'Fast-food' },
+  { value: 'Oyster Bar', name: 'Oyster Bar' },
+  { value: 'Brew Pub', name: 'Brew Pub' },
+  { value: 'Bistro', name: 'Bistro' },
+  { value: 'Steakhouse', name: 'Steakhouse' }
+]
+
+const RestaurantForm = ({ restaurant, handleChange, handleSubmit, cancelPath }) => (
+  <form onSubmit={handleSubmit}>
+    <label>Restaurant</label>
+    <input
+      name='title'
+      placeholder='Restaurant Name'
+      value={restaurant.title}
+      onChange={handleChange}
+    />
+
+    <label>Type of Food</label>
+    <select
+      name='foodtype'
+      value={restaurant.foodtype}
+      onChange={handleChange}
+    >
+      {categoryData.map((event, value) => {
+        return <option key={value}>{event.name}</option>
+      })}
+    </select>
+
+    <label>Address</label>
+    <input
+      name='address'
+      placeholder='Address'
+      value={restaurant.address}
+      onChange={handleChange}
+    />
+
+    <label>City</label>
+    <input
+      name='city'
+      placeholder='City'
+      value={restaurant.city}
+      onChange={handleChange}
+    />
+
+    <label>State</label>
+    <input
+      name='state'
+      placeholder='State'
+      value={restaurant.state}
+      onChange={handleChange}
+    />
+
+    <label>Zip Code</label>
+    <input
+      name='zipcode'
+      type='number'
+      placeholder='Zip Code'
+      value={restaurant.zipcode}
+      onChange={handleChange}
+    />
+
+    <button type='submit'>Submit</button>
+    <Link to={cancelPath}>
+      <button>Cancel</button>
+    </Link>
+
+  </form>
+)
+
+export default RestaurantForm
