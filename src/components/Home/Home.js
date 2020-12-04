128d5cde8eb36399193e505b17669c1978d965b7
Scott Solmonson <ssolmonson63@gmail.com>
2020-12-03 23:46:48 -0500

    Add temp message to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
 const Home = () => (
-  <div></div>
+  <div>
+    <p>Welcome! Page Currently Under Development.<br /> Please check back soon!</p>
+  </div>
 )
 
 export default Home
