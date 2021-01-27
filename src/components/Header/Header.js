d2ed3126fb9391877ab6a9cd978c4ca913148e3e
Scott Solmonson <ssolmonson63@gmail.com>
2021-01-26 21:04:54 -0500

    Edit wording on nav link for restaurants page
    Altered the wording slightly for a slightly more defined labeling of the
    page.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const authenticatedOptions = (
   <Fragment>
     <Nav.Link href="#change-password">Change Password</Nav.Link>
     <Nav.Link href="#sign-out">Sign Out</Nav.Link>
-    <Nav.Link href="#restaurants">Show your restaurants</Nav.Link>
+    <Nav.Link href="#restaurants">View your restaurants</Nav.Link>
   </Fragment>
 )
 
