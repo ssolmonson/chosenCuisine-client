6d0e28a0244936f1ca949fa09df3f16d03e675b5
Scott Solmonson <ssolmonson63@gmail.com>
2021-01-26 21:16:17 -0500

    Add file for map component and notes
    Created a folder and file for the Leaflet map component. Pseudo code
    added in file to note on structure and possible ideas moving forward.

---
+++ src/components/CuisineMap/CuisineMap.js
@@ -0,0 +1,9 @@
+// Start of structure for the map component
+// Plan to use Leaflet to map out the restaurant locations
+
+// Consider map on restaurants page or just using map component within individual restaurant ids
+// This could just be done when rendering each location
+
+// Note: Still need to review and define how the address will be entered
+// Look into if it's better to change how the address is stored in the database:
+// {Address: Street, City, State, Zip} or pulled for each piece as it's setup right now.
